915ee2d80f9cac7dcda31435b8a9d3158f75d079
Ben Marwick <benmarwick@gmail.com>
2013-07-28 10:59:44 -0700

    update scraper

--- 001_scrape_for_links_to_fulltext.r
+++ 001_scrape_for_links_to_fulltext.r
@@ -5,14 +5,17 @@
 library(RCurl)
 library(XML)
 
-n <- 65 # determined by inspecting the first page
+n <- 100 # determined by inspecting the first page
 # pre-allocate list to fill
 links <- vector("list", length = n)
+# get URLs from the first page separately, since the URL for
+# the first page doesn't follow a pattern
+links[[1]] <-  unname(xpathSApply(htmlParse(getURI("http://www.dayofarchaeology.com/")),"//h2/a/@href"))
 for(i in 1:n){
   # track progress by showing the iteration we're up to
   print(i)
-  # get all content on the i-th page of the main blog list
+  # get all content on the i+1 th page of the main blog list
   blogdata <- htmlParse(getURI(paste0("http://www.dayofarchaeology.com/page/", i+1,"/")))
   # extract links for all posts
-  links[[i]] <- unname(xpathSApply(blogdata,"//h2/a/@href"))
+  links[[i+1]] <- unname(xpathSApply(blogdata,"//h2/a/@href"))
 }
